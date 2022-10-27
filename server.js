56eaf71c8294608c765730daac0911fb0bf08689
t-onyango <t.onyango@alustudent.com>
2022-10-27 20:40:43 +0200

    my files

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', function(req, res){
+    res.json({slackUsername: 'topingstar', backend: true, age : 24, bio: 'I am Topister, 24 years old. Currently a computer science student at African Leadership University. I am more focused in Backend development.I much interested in learning new technology.'})
+
+})
+// app.use(cors({
+//     origin: '*'
+// })); 
+
+app.listen(PORT, () =>{
+    console.log(`Listening on port ${PORT}`)
+})
